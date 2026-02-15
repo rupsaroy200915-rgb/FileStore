8dd8f71a4c00a9b84cdc7bad227ad4a9d65350fa
rupsaroy200915-rgb <rupsaroy200915@gmail.com>
2026-02-15 20:44:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ DB_NAME = "Ram"
 
 FSUBS = [[-1003751570614, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL = -1003578999463   # just put channel id dont add ""
+#DB_CHANNEL = -1003578999463   # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
 #     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
